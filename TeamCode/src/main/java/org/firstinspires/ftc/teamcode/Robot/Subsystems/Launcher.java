50b240cf31fe20a8ced896720000a99baf7d73d2
blenderbot_new_mac1 <team12492@gmail.com>
2026-09-18 12:53:37 -0700

    turret code misssmatch fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Subsystems/Launcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Subsystems/Launcher.java
@@ -1,4 +1,28 @@
 package org.firstinspires.ftc.teamcode.Robot.Subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class Launcher {
+
+    private DcMotor launchWheel1;
+    private DcMotor launchWheel2;
+
+    //initialize
+    public Launcher(DcMotor LaunchWheel1, DcMotor LaunchWheel2) {
+        launchWheel1 = LaunchWheel1;
+        launchWheel2 = LaunchWheel2;
+    }
+
+    //turn on launch wheels
+    public void launch() {
+        launchWheel1.setPower(0.97);
+        launchWheel2.setPower(0.95);
+    }
+
+    //turn off launch wheels
+    public void stopLaunch() {
+        launchWheel1.setPower(0);
+        launchWheel2.setPower(0);
+    }
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Subsystems/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Subsystems/Turret.java
@@ -3,25 +3,6 @@ package org.firstinspires.ftc.teamcode.Robot.Subsystems;
 import com.qualcomm.robotcore.hardware.DcMotor;
 public class Turret {
 
-    private DcMotor launchWheel1;
-    private DcMotor launchWheel2;
 
-    //initialize
-    public Turret(DcMotor LaunchWheel1, DcMotor LaunchWheel2) {
-        launchWheel1 = LaunchWheel1;
-        launchWheel2 = LaunchWheel2;
-    }
-
-    //turn on launch wheels
-    public void launch() {
-        launchWheel1.setPower(1);
-        launchWheel2.setPower(1);
-    }
-
-    //turn off launch wheels
-    public void stopLaunch() {
-        launchWheel1.setPower(0);
-        launchWheel2.setPower(0);
-    }
 
 }
